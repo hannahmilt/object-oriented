7886613648acdcaa8831372d441f1b8ee68edf38
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-04-09 15:34:04 -0600

    added fake info to test

--- php/classes/Test/AuthorTest.php
+++ php/classes/Test/AuthorTest.php
@@ -12,6 +12,21 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
 	class AuthorTest extends DataDesignTest {
 
+	//this is fake data so we can TEST
+	private $VALID_AUTHOR_ACTIVATION_TOKEN;//this will be done in the setup
+	private $VALID_AUTHOR_AVATAR_URL = "https://avatar.com";
+	private $VALID_AUTHOR_EMAIL = "hannahmilt@gmail.com";
+	private $VALID_AUTHOR_HASH; //THIS WILL BE DONE IN THE SETUP
+	private $VALID_AUTHOR_USERNAME = "hmiltenberger";
+
+	public function setUp() : void {
+		parent::setUp();
+
+		$password ="my_super_secret_password";
+		$this->VALID_AUTHOR_HASH = password_hash( "password",PASSWORD_ARGON2I, ["time_cost" => 45]);
+		$this->VALID_AUTHOR_ACTIVATION_TOKEN = bin2hex(random_bytes(16));
+	}
+
 	public function testInsertValidAuthor() : void {
 
 	}
